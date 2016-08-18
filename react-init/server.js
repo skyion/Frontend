57ccd3dd9822fb39eca18ff80835dc83f29d7306
Rj <butcher@mail.ru>
2016-08-18 14:07:51 +0300

    Added react-init

---
+++ react-init/server.js
@@ -0,0 +1,10 @@
+var webpack          = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config           = require('./webpack.config');
+
+
+new WebpackDevServer(webpack(config), {
+    publicPath        : config.output.publicPath,
+    hot               : true,
+    historyApiFallback: true
+}).listen(3000, 'localhost');
---
+++ react-init/src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+console.clear();
+export default class App extends Component {
+    render() {
+        return (
+            <h1>Hello, world!</h1>
+        );
+    }
+}
